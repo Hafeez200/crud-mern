49aff9c7c9cb06a3ec13609fbe04b936b5ad083a
Hafeez200 <saibhaji24@email.com>
2024-10-12 14:13:14 +0500

    add schema

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 const mongoURI = "mongodb://localhost:27017/test";
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB connection error:", error));
+  
+  const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    age: { type: Number, required: true },
+  });
+  
+  const User = mongoose.model("User", userSchema);
